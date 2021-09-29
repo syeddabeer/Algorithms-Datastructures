a88cd7cdeebfc9d955bd8bd430872287fef97b8f
Dabeeruddin Syed <syeddabeeruddin56@gmail.com>
2021-09-29 16:18:10 -0500

    binary tree

---
+++ datastructures/binaryTreeZigZagTraversal.py
@@ -0,0 +1,41 @@
+from collections import deque
+
+class Solution:
+    def zigzagLevelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        ret = []
+        ret2 = deque()
+        if root is None:
+            return []
+        # start with the level 0 with a delimiter
+        node_queue = deque([root, None])
+        is_order_left = True
+
+        while len(node_queue) > 0:
+            curr_node = node_queue.popleft()
+
+            if curr_node:
+                if is_order_left:
+                    ret2.append(curr_node.val)
+                else:
+                    ret2.appendleft(curr_node.val)
+
+                if curr_node.left:
+                    node_queue.append(curr_node.left)
+                if curr_node.right:
+                    node_queue.append(curr_node.right)
+            else:
+                # we finish one level
+                ret.append(ret2)
+                # add a delimiter to mark the level
+                if len(node_queue) > 0:
+                    node_queue.append(None)
+
+                # prepare for the next level
+                ret2 = deque()
+                is_order_left = not is_order_left
+
+        return ret
