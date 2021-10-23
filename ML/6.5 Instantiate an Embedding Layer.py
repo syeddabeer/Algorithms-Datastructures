2b27d25af9262945d475ef08cca431e15cb75e31
Dabeeruddin Syed <syeddabeeruddin56@gmail.com>
2021-10-23 15:10:07 -0500

    Delete ML directory

--- ML/6.5 Instantiate an Embedding Layer.py
+++
@@ -1,5 +0,0 @@
-from keras.layers import Embedding
-
-embedding_layer = Embedding(1000, 64)
-# 1000 - 1+ max word index
-# dimensionality here is 64
--- ML/temp.py
+++
@@ -1,5 +0,0 @@
-
-
-# in convnets, depth of feature maps increases down the model
-# the size of feature maps decreases down the model
-import spacy
