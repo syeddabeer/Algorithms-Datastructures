bdda61f1782e883284fbf01d095c1c598b258321
Dabeeruddin Syed <syeddabeeruddin56@gmail.com>
2021-11-16 23:59:08 -0600

    ml

--- ds/Reverse Linked List II.py
+++ ds/Reverse Linked List II.py
@@ -7,11 +7,44 @@ reverse the nodes of the list from position left to position right, and return t
 # 	def __init__(self, val=0, next=None):
 # 		self.val = val 
 # 		self.next = next
+# prev=0, cur = 1
+# tail aage aur con peeche.
 class Solution:
-	def reverseBetween(self, head, m, n):
-		if not head:
-			return None
-		# Move the two pointers until they reach the proper starting point
-        # in the list.
+    def reverseBetween(self, head, m, n):
+        """
+        :type head: ListNode
+        :type m: int
+        :type n: int
+        :rtype: ListNode
+        """
 
+        # Empty list
+        if not head:
+            return None
+
+        # Move the two pointers until they reach the proper starting point
+        # in the list.
         cur, prev = head, None
+        while m > 1:
+            prev = cur
+            cur = cur.next
+            m, n = m - 1, n - 1
+
+        # The two pointers that will fix the final connections.
+        tail, con = cur, prev
+
+        # Iteratively reverse the nodes until n becomes 0. # i.e. from m to n
+        while n:
+            third = cur.next
+            cur.next = prev
+            prev = cur
+            cur = third
+            n -= 1
+
+        # Adjust the final connections as explained in the algorithm
+        if con:
+            con.next = prev
+        else:
+            head = prev
+        tail.next = cur
+        return head
