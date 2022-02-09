21b705adb371159b0e340de7ff9349ec7d4194bb
Syed <dabsyed@deloitte.com>
2022-02-08 20:18:10 -0600

    personal file

---
+++ 252. Meeting Rooms.py
@@ -0,0 +1,12 @@
+class Solution:
+    def canAttendMeetings(self, intervals):
+        
+"""
+Given an array of meeting time intervals where intervals[i] = [starti, endi], determine if a person could attend all meetings.
+Example 1:
+Input: intervals = [[0,30],[5,10],[15,20]]
+Output: false
+Example 2:
+Input: intervals = [[7,10],[2,4]]
+Output: true
+"""
