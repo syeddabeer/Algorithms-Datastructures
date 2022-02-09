83f487644311596b1fbb1f5cf3258db152fd2252
Syed <dabsyed@deloitte.com>
2022-02-09 00:05:07 -0600

    personal file

--- 344. Reverse String.py
+++ 344. Reverse String.py
@@ -4,7 +4,7 @@ class Solution(object):
         :type s: List[str]
         :rtype: None Do not return anything, modify s in-place instead.
         """
-        
+        s.reverse()
 """
 Write a function that reverses a string. The input string is given as an array of characters s.
 You must do this by modifying the input array in-place with O(1) extra memory.
