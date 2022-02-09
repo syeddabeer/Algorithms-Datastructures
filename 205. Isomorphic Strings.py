258267737cf20c9d0208ef46fe34f467e595045c
Syed <dabsyed@deloitte.com>
2022-02-08 22:27:51 -0600

    personal file

--- 205. Isomorphic Strings.py
+++ 205. Isomorphic Strings.py
@@ -1,4 +1,19 @@
-
+class Solution:
+    def isIsomorphic(self, s, t):
+        dict_s_to_t = {}
+        dict_t_to_s = {}
+        
+        for i, j in zip(s, t):
+            if i not in dict_s_to_t and j not in dict_t_to_s:
+                dict_s_to_t[i]=j
+                dict_t_to_s[j]=i
+            
+            # elif dict_s_to_t[i]!=j or dict_t_to_s[j]!=i:
+            elif dict_s_to_t.get(i) != j or dict_t_to_s.get(j) != i:
+                return False
+        
+        return True
+          
 """
 Given two strings s and t, determine if they are isomorphic.
 
