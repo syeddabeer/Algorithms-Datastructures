ed8c1048701f233d60fe143a47f6537d311067f9
Syed <dabsyed@deloitte.com>
2022-02-08 23:48:54 -0600

    personal file

--- 125. Valid Palindrome.py
+++ 125. Valid Palindrome.py
@@ -1,4 +1,18 @@
-
+class Solution():
+    def isPalindrome(self, s):
+        left = 0
+        right = len(s)-1
+        
+        while left < right:
+            while left < right and not s[left].isalnum():
+                left += 1
+            while left < right and not s[right].isalnum():
+                right -= 1
+            if s[left].lower() != s[right].lower():
+                return False
+            left+=1
+            right-=1
+        return True
 """
 A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
 Given a string s, return true if it is a palindrome, or false otherwise.
