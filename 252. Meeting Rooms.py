230862787151020bf607e23b1f53462f4e4849c3
Syed <dabsyed@deloitte.com>
2022-02-08 20:26:16 -0600

    personal file

--- 252. Meeting Rooms.py
+++ 252. Meeting Rooms.py
@@ -1,6 +1,16 @@
+# method 2
+
+# brute force
 class Solution:
     def canAttendMeetings(self, intervals):
+        def overlap(interval1, interval2):
+            return (interval1[0]>=interval2[0] and interval1[0]<interval2[1] or interval2[0]>=interval1[0] and interval2[0]<interval1[1])
         
+        for i in range(len(intervals)):
+            for j in range(i+1, len(intervals)):
+                if overlap(intervals[i], intervals[j]):
+                    return False
+        return True
 """
 Given an array of meeting time intervals where intervals[i] = [starti, endi], determine if a person could attend all meetings.
 Example 1:
