035cbd6c4b1bb8f6f10f284f400c39f22208480d
Dabeeruddin Syed <syeddabeeruddin56@gmail.com>
2022-02-27 14:59:21 -0600

    ml

--- trial.py
+++ trial.py
@@ -1,22 +1,29 @@
-class TrieNode:
-    def __init__(self):
-        self.children={}
-        self.isEnd=False 
+class LinkedNode:
+    def __init__(self, value, next=None, previous=None):
+        self.value = value
+        self.next = next 
+        self.previous = previous
 
-class Trie:
+class DoublyLinkedList:
     def __init__(self):
-        self.root = TrieNode()
-    
-    def insert(self, word):
-        node = self.root 
-        for char in word:
-            if char not in node.children:
-                node.children[char] = TrieNode()
-                
-        
-    def search:
-    
-    def startswith:
-    
-    def remove:
-    
+        self.head = None 
+
+    def insertAtStart(self, value):
+        if self.head == None:
+            self.head = LinkedNode(value)
+        else:
+            newNode = LinkedNode(value)
+            newNode.next = self.head 
+            self.head.previous = newNode
+            self.head = newNode
+
+    def insertAtEnd(self, value):
+        newNode = LinkedNode(value)
+
+        temp = self.head 
+        while temp.next != Node
+            temp=temp.next 
+        temp.next = newNode
+
+
+    def delete(self, value):
