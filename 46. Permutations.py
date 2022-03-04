61f17bcbfe7379c743d6cb896befb95c6bfd8160
Syed <dabsyed@deloitte.com>
2022-03-04 12:52:58 -0600

    personal file

--- 46. Permutations.py
+++ 46. Permutations.py
@@ -1,5 +1,20 @@
 #46. Permutations
-
+class Solution:
+    def permute(self, nums):
+        output=[]
+        n=len(nums)
+        
+        def backtrack(first=0):
+            if first==n:
+                output.append(nums[:])
+            for i in range(first, n):
+                nums[first], nums[i] = nums[i], nums[first]
+                backtrack(first+1)
+                nums[first], nums[i] = nums[i], nums[first]
+            
+        backtrack()
+        
+        return output
 
 """
 Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
