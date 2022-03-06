6b2e04d14d5253bc4ea0dbf7856bd90bc65f19fb
Dabeeruddin Syed <syeddabeeruddin56@gmail.com>
2022-03-05 20:56:13 -0600

    ml

--- trial.py
+++ trial.py
@@ -1,17 +1,29 @@
-class NestedIterator:
-    def __init__(self, nestedList):
-        def flatten(nl):
-            tmp=[]
-            for i in nl:
-                if i.isInteger():
-                    tmp.append(i)
-                else:
-                    tmp.extend(flatten(i.getList()))
-            return tmp
+class Solution:
+    def candyCrush(self, M):
+        while True:
+            #break
+            #1. compare
+            crush = set()
+            for i in range(len(M)):
+                for j in range(len(M[0])):
+                    if i>1 and M[i][j] and M[i][j]==M[i-1][j]==M[i-2][j]:
+                        crush |= {(i,j), (i-1, j), (i-2, j)}
+                    if j>1 and M[i][j] and M[i][j]==M[i][j-1]==M[i][j-2]:
+                        crush |= {(i,j), (i,j-1), (i,j-2)}
 
-        self.n = flatten(nestedList)
-    def next(self):
-        return self.n.pop(0)
+            #2. crush
+            if not crush:
+                break
+            for i, j in crush:
+                M[i][j]=0
 
-    def hasNext(self):
-        return len(self.n)>0
+            # 3. drop
+            for j in range(len(M[0])):
+                idx=len(M)-1
+                for i in reversed(range(len(M))):
+                    if M[i][j]: 
+                        M[idx][j]=M[i][j]
+                        idx-=1
+                for i in range(idx+1): M[i][j]=0
+
+        return M
