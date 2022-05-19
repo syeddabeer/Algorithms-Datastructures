a2729cafc5d86b44f65eb48ab945fb70d342d332
Dabeeruddin Syed <syeddabeeruddin56@gmail.com>
2022-05-18 23:43:57 -0500

    ml

---
+++ 1041. Robot Bounded In Circle.py
@@ -0,0 +1,92 @@
+#robot bounded by circle after given instructions as L R G - g for go ahead
+class robotBound:
+    def isRobotBounded(self, instructions):
+        
+        #initial point or origin
+        x = 0
+        y = 0
+        direction = 0
+        
+        #[0,1]-> NORTH :::: [1, 0]-> EAST :::: [0, -1]-> SOUTH :::: [-1, 0]-> WEST
+        coordinates=[[0,1],[1,0],[0,-1],[-1,0]]
+        
+        for i in instructions:
+            if i == "L":
+                direction=(direction+3)%4 #at every 4 steps NEWS resets to 0. reset call for %4
+            elif i == "R":
+                direction=(direction+1)%4
+            else:
+                x += coordinates[direction][0]
+                y += coordinates[direction][1]
+        
+        # after one cycle:
+        # robot returns into initial position
+        # or robot doesn't face north
+        return (x == 0 and y == 0) or direction != 0
+
+instructions="GGGLGLGLGGL"
+answer=robotBound().isRobotBounded(instructions)
+print(answer)
+
+"""
+On an infinite plane, a robot initially stands at (0, 0) and faces north. Note that:
+
+The north direction is the positive direction of the y-axis.
+The south direction is the negative direction of the y-axis.
+The east direction is the positive direction of the x-axis.
+The west direction is the negative direction of the x-axis.
+The robot can receive one of three instructions:
+
+"G": go straight 1 unit.
+"L": turn 90 degrees to the left (i.e., anti-clockwise direction).
+"R": turn 90 degrees to the right (i.e., clockwise direction).
+The robot performs the instructions given in order, and repeats them forever.
+
+Return true if and only if there exists a circle in the plane such that the robot never leaves the circle.
+
+ 
+
+Example 1:
+
+Input: instructions = "GGLLGG"
+Output: true
+Explanation: The robot is initially at (0, 0) facing the north direction.
+"G": move one step. Position: (0, 1). Direction: North.
+"G": move one step. Position: (0, 2). Direction: North.
+"L": turn 90 degrees anti-clockwise. Position: (0, 2). Direction: West.
+"L": turn 90 degrees anti-clockwise. Position: (0, 2). Direction: South.
+"G": move one step. Position: (0, 1). Direction: South.
+"G": move one step. Position: (0, 0). Direction: South.
+Repeating the instructions, the robot goes into the cycle: (0, 0) --> (0, 1) --> (0, 2) --> (0, 1) --> (0, 0).
+Based on that, we return true.
+Example 2:
+
+Input: instructions = "GG"
+Output: false
+Explanation: The robot is initially at (0, 0) facing the north direction.
+"G": move one step. Position: (0, 1). Direction: North.
+"G": move one step. Position: (0, 2). Direction: North.
+Repeating the instructions, keeps advancing in the north direction and does not go into cycles.
+Based on that, we return false.
+Example 3:
+
+Input: instructions = "GL"
+Output: true
+Explanation: The robot is initially at (0, 0) facing the north direction.
+"G": move one step. Position: (0, 1). Direction: North.
+"L": turn 90 degrees anti-clockwise. Position: (0, 1). Direction: West.
+"G": move one step. Position: (-1, 1). Direction: West.
+"L": turn 90 degrees anti-clockwise. Position: (-1, 1). Direction: South.
+"G": move one step. Position: (-1, 0). Direction: South.
+"L": turn 90 degrees anti-clockwise. Position: (-1, 0). Direction: East.
+"G": move one step. Position: (0, 0). Direction: East.
+"L": turn 90 degrees anti-clockwise. Position: (0, 0). Direction: North.
+Repeating the instructions, the robot goes into the cycle: (0, 0) --> (0, 1) --> (-1, 1) --> (-1, 0) --> (0, 0).
+Based on that, we return true.
+ 
+
+Constraints:
+
+1 <= instructions.length <= 100
+instructions[i] is 'G', 'L' or, 'R'.
+"""
